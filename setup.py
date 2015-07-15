4bd9f467cf07329016a0e7f0c35686c01788d88c
BillMills <mills.wj@gmail.com>
2015-07-15 16:10:08 -0700

    first post

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wodpy',
+    version='0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A parser for the WOD data format, described in http://data.nodc.noaa.gov/woa/WOD/DOC/wodreadme.pdf',
+    long_description=open('README.md').read(),
+    install_requires=['copy', 'numpy', 'os'],
+    url='',
+    author='Simon Good, Bill Mills',
+    author_email='mills.wj@gmail.com'
+)
