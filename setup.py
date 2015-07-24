4b5d51b237bbb2b3326ae845f8c5829e7455f2b4
BillMills <mills.wj@gmail.com>
2015-07-24 13:18:52 -0400

    dep tweak

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license='MIT',
     description='A parser for the WOD data format, described in http://data.nodc.noaa.gov/woa/WOD/DOC/wodreadme.pdf',
     long_description=open('README.md').read(),
-    install_requires=['numpy', 'os'],
+    install_requires=['numpy'],
     url='',
     author='Simon Good, Bill Mills',
     author_email='mills.wj@gmail.com'
