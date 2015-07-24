d3d969dd15d982670ab07c78336458f23b06b9e6
BillMills <mills.wj@gmail.com>
2015-07-24 13:28:00 -0400

    add website link

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='A parser for the WOD data format, described in http://data.nodc.noaa.gov/woa/WOD/DOC/wodreadme.pdf',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='',
+    url='https://github.com/BillMills/wodpy',
     author='Simon Good, Bill Mills',
     author_email='mills.wj@gmail.com'
 )
